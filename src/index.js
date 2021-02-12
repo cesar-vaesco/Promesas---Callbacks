11c8a7085d550e18fdf40d021bd00e587d121ede
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-11 23:58:12 -0600

    Pro-ca: conociendo las promesas

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
-import { buscarHeroe, saludar } from './js/callbacks';
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
+import { buscarHeroe } from './js/promesas';
 import './styles.css'
 
 const heroeId1 = 'capi';
 const heroeId2 = 'spider';
 const heroeId3 = 'iron';
 
+
+
+
+
 /*-- implementación de la función buscarHeroe --*/
 // Callback: función que es enviada como argumento
-buscarHeroe(heroeId1, (err, heroe1) => {
+// buscarHeroe(heroeId1, (err, heroe1) => {
 
-  if (err) { return console.error(err); }
-
-
-  buscarHeroe(heroeId2, (err, heroe2) => {
-
-    if (err) { return console.error(err); }
+//   if (err) { return console.error(err); }
 
 
-    buscarHeroe(heroeId2, (err, heroe3) => {
+//   buscarHeroe(heroeId2, (err, heroe2) => {
 
-      if (err) { return console.error(err); }
+//     if (err) { return console.error(err); }
 
-      console.log(`Enviado a ${heroe1.nombre}, a ${heroe2.nombre}  y  ${heroe3.nombre}a la misión`);
 
-    });
-  });
+//     buscarHeroe(heroeId3, (err, heroe3) => {
 
+//       if (err) { return console.error(err); }
+
+//       console.log(`Enviado a ${heroe1.nombre}, a ${heroe2.nombre}  y  ${heroe3.nombre}a la misión`);
+
+//     });
+//   });
+
+// });
+
+buscarHeroe( heroeId1).then(heroe =>{
+  console.log(`Enviado ${heroe.nombre } a la misión`);
 });
 
-console.log('Fin del programa....')
+console.log('Fin del programa....');
 
 // Un callback siempre regresa primero el error y después la funcionalidad
