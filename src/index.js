5b4224bd4f9365d38a25a3455a66e439399b81df
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-11 23:42:38 -0600

    Pro-ca: ejemplo de Callback hell

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import { buscarHeroe,saludar } from './js/callbacks';
+import { buscarHeroe, saludar } from './js/callbacks';
 import './styles.css'
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+const heroeId3 = 'iron';
 
- /*-- implementación de la función buscarHeroe --*/
+/*-- implementación de la función buscarHeroe --*/
 // Callback: función que es enviada como argumento
-buscarHeroe ( heroeId, (err, heroe) => {
+buscarHeroe(heroeId1, (err, heroe1) => {
 
-  if (err) {
-    console.error(err);
+  if (err) { return console.error(err); }
 
-  }else {
-    console.log(heroe);
-  }
-} );
 
+  buscarHeroe(heroeId2, (err, heroe2) => {
+
+    if (err) { return console.error(err); }
+
+
+    buscarHeroe(heroeId2, (err, heroe3) => {
+
+      if (err) { return console.error(err); }
+
+      console.log(`Enviado a ${heroe1.nombre}, a ${heroe2.nombre}  y  ${heroe3.nombre}a la misión`);
+
+    });
+  });
+
+});
+
+console.log('Fin del programa....')
 
 // Un callback siempre regresa primero el error y después la funcionalidad
