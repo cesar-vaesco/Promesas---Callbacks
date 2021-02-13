894cfd7180f96bb8474ad9613853fe590f5c0192
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-13 08:15:59 -0600

    Pro-ca: manejo del error con await

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { obtenerHeroesArr } from "./js/await";
+import { obtenerHeroesArr, obtenerHeroeAwait } from "./js/await";
 
 
 console.time('await');
-obtenerHeroesArr().then(heroes => {
-       console.table(heroes);
-       console.timeEnd('await');
-});
+
+obtenerHeroeAwait( 'capi2')
+       .then(heroe => {
+              console.log(heroe);
+              console.timeEnd('await');
+}).catch(console.warn());
 
 // console.log('Heroes: ', heroes);
 // console.log( heroes);
--- src/js/await.js
+++ src/js/await.js
@@ -8,18 +8,23 @@ export const obtenerHeroesArr = async() => {
 
   return await Promise.all(heroesIds.map( buscarHeroe ));
 
-  // const heroesArr = [];
-
-  // for (const id of heroesIds) {
-
-  //   heroesArr.push(buscarHeroe(id));
-
-  // }
-
-  // // setTimeout(() => {
-  // //   console.log('Obtener heroes');
-  // //   console.log( heroesArr);
-  // // }, 1000);
-
-  // return await Promise.all(heroesArr);
 };
+
+
+export const obtenerHeroeAwait = async (id) => {
+
+  try {
+    const heroe = await buscarHeroeAsync( id );
+    return heroe;
+
+  } catch (err) {
+    console.log('CATCH!!!');
+    return {
+      nombre: 'SIN NOMBRE',
+      poder: 'sin poder'
+    }
+  }
+
+
+
+}
