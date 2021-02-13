fb7bb9d47eed6d867a8e4ecc2ec936363e5c506e
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-13 07:50:23 -0600

    Pro-ca: usando el await en una función

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-import { buscarHeroe, buscarHeroeAsync } from './js/promesas';
+import { obtenerHeroesArr } from "./js/await";
 
 
-// promesaLenta.then(mensaje => console.log(mensaje));
-// promesaLenta.then(console.log);
-// promesaMedia.then(console.log);
-// promesaRapida.then(console.log);
 
-// Promise.race([promesaLenta, promesaMedia, promesaRapida])
-//        .then(mensaje => console.log(mensaje));
+ obtenerHeroesArr().then(console.table);
 
-//  Promise.race resibe varias promesas y resuelve la primera que se resuelve
-
-buscarHeroe('capi')
-       .then(heroe => console.log(heroe))
-       .catch(console.warn);//manejo de errores
-buscarHeroeAsync('iron2')
-       .then(heroe => console.log(heroe))
-       .catch(console.warn);//manejo de errores
+// console.log('Heroes: ', heroes);
+// console.log( heroes);
